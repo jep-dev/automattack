5c7d22391cfca6f38c723a8403a5fc648ae38968
jep-dev <john@xpcx.com>
2023-03-04 13:47:38 -0500

    Initial commit with boilerplate

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+__author__ = "John Petersen"
+__version__ = "0.0.1"
+__license__ = "GNU General Public License 3.0"
+
+def main():
+    print("hello world")
+    return
+
+if __name__ == "__main__":
+    main()
