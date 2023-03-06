1ea50e797e8bedef62ce72632c3930da295e32f3
jep-dev <john@xpcx.com>
2023-03-06 11:51:43 -0500

    Removed Animate/Inanimate

--- char.py
+++ char.py
@@ -5,38 +5,32 @@ __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
 
 class Stats:
-    health = 1
-    def __init__(self, health):
-        self.health = health
-
-class Animate(Stats):
-    armor = 1
-    dexterity = 1
-    intelligence = 1
-    strength = 1
-    mresistance = 1
-    def __init__(self, armor, dexterity,
-            health, intelligence, mresistance, strength):
-        super().__init__(self, health)
+    armor = 0
+    dexterity = 0
+    health = 0
+    initiative = 0
+    intelligence = 0
+    mresistance = 0
+    strength = 0
+    weight = 0
+    def __init__(self, armor, dexterity, health, initiative,
+            intelligence, mresistance, strength, weight):
         self.armor = armor
         self.dexterity = dexterity
+        self.health = health
+        self.initiative = initiative
         self.intelligence = intelligence
         self.mresistance = mresistance
         self.strength = strength
-
-class Inanimate(Stats):
-    weight = 1
-    def __init__(self, health, weight):
-        super().__init__(self, health)
         self.weight = weight
 
-class Char(Animate):
+class Char(Stats):
     name = "Greg"
-    species = "gregoid"
-    def __init__(self, name, species, armor, dexterity,
-            health, intelligence, mresistance, strength):
-        super().__init__(self, armor, dexterity, health,
-                intelligence, mresistance, strength)
+    species = "base"
+    def __init__(self, name, species, armor, dexterity, health,
+            initiative, intelligence, mresistance, strength):
+        super().__init__(armor, dexterity, health, initiative,
+                intelligence, mresistance, strength, 10)
         self.name = name
         self.species = species
     def __str__(self):
@@ -44,15 +38,15 @@ class Char(Animate):
 
 class Human(Char):
     def __init__(self, name):
-        super().__init__(self, name, "human",
-                10, 10, 10, 10, 10)
+        super().__init__(name, "human",
+                10, 10, 10, 10, 10, 10, 10)
 
 class Orc(Char):
     def __init__(self, name):
-        super().__init__(self, name, "orc",
-                10, 10, 8, 8, 14)
+        super().__init__(name, "orc",
+                10, 10, 10, 12, 8, 8, 12)
 
 class Elf(Char):
     def __init__(self, name):
-        super().__init__(self, name, "elf",
-                10, 10, 8, 14, 8)
+        super().__init__(name, "elf",
+                10, 10, 10, 8, 12, 12, 8)
