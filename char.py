c153895b891dd40cb22dd6e4b64b9174b8fc45ec
jep-dev <john@xpcx.com>
2023-03-07 05:59:05 -0500

    Moved stats from the base class to an independent one for backups

--- char.py
+++ char.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from copy import copy, deepcopy
+
 __author__ = "John Petersen"
 __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
@@ -12,9 +14,8 @@ class Stats:
     intelligence = 0
     mresistance = 0
     strength = 0
-    weight = 0
     def __init__(self, armor, dexterity, health, initiative,
-            intelligence, mresistance, strength, weight):
+            intelligence, mresistance, strength):
         self.armor = armor
         self.dexterity = dexterity
         self.health = health
@@ -22,31 +23,39 @@ class Stats:
         self.intelligence = intelligence
         self.mresistance = mresistance
         self.strength = strength
-        self.weight = weight
 
-class Char(Stats):
+class Char:
     name = "Greg"
     species = "base"
-    def __init__(self, name, species, armor, dexterity, health,
-            initiative, intelligence, mresistance, strength):
-        super().__init__(armor, dexterity, health, initiative,
-                intelligence, mresistance, strength, 10)
+
+    base_stats = None
+    stats = None
+
+    target = None
+
+    def __str__(self):
+        return f'{self.name} the {self.species}'
+    def __init__(self, name, species, stats):
         self.name = name
         self.species = species
-    def __str__(self):
-        return f"{self.name} the {self.species}"
+        self.base_stats = deepcopy(stats)
+        self.stats = stats
 
 class Human(Char):
     def __init__(self, name):
-        super().__init__(name, "human",
-                10, 10, 10, 10, 10, 10, 10)
+        super().__init__(name, "human", Stats(
+            4, 10, 10, 10, 10, 10, 10))
+        # .2, .5, .5, .5, .5, .5, .5))
 
 class Orc(Char):
     def __init__(self, name):
-        super().__init__(name, "orc",
-                10, 10, 10, 12, 8, 8, 12)
+        super().__init__(name, "orc", Stats(
+            5, 10, 10, 11, 8, 8, 12))
+        # .25, .5, .5, .55, .4, .4, .6))
 
 class Elf(Char):
     def __init__(self, name):
-        super().__init__(name, "elf",
-                10, 10, 10, 8, 12, 12, 8)
+        super().__init__(name, "elf", Stats(
+                4, 10, 10, 9, 10, 12, 9))
+        # .2, .5, .5, .45, .5, .6, .45))
+
