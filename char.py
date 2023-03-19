9892c0cca86e77f0d52e91068d52b30ced96787f
jep-dev <john@xpcx.com>
2023-03-19 01:17:11 -0400

    Made stats' constructor relative

--- char.py
+++ char.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python3
 
 from copy import copy, deepcopy
-
+from util import *
 
 __author__ = "John Petersen"
 __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
 
 class Stats:
-    def __init__(self, ARM, DEX, HP, INV, INT, MR, STR):
-        self.ARM = ARM
-        self.DEX = DEX
-        self.HP = HP
-        self.INV = INV
-        self.INT = INT
-        self.MR = MR
-        self.STR = STR
+    def __init__(self, ARM=0, DEX=0, HP=0, \
+            INV=0, INT=0, MR=0, STR=0):
+        self.ARM = clamp(4+ARM)
+        self.DEX = clamp(10+DEX)
+        self.HP = clamp(100+HP)
+        self.INV = clamp(10+INV)
+        self.INT = clamp(10+INT)
+        self.MR = clamp(10+MR)
+        self.STR = clamp(10+STR)
 
 class Char:
     name = "Greg"
     kin = "base"
-    died = False
-
     stats = None
     base_stats = None
-
     target = None
 
     def __str__(self):
         return f'{self.name} the {self.kin}'
-    def __init__(self, name, kin, stats):
+    def __init__(self, name, kin, stats = Stats()):
         self.name = name
         self.kin = kin
         self.stats = stats
@@ -37,33 +35,46 @@ class Char:
 
 class Human(Char):
     def __init__(self, name):
-        super().__init__(name, "human", Stats(
-            4, 10, 10, 10, 10, 10, 10))
+        super().__init__(name, "human")
 
 class Orc(Char):
     def __init__(self, name):
         super().__init__(name, "orc", Stats(
-            5, 10, 10, 11, 8, 8, 12))
+            1, 0, 0, 1, -2, -2, 2))
 
 class Elf(Char):
     def __init__(self, name):
         super().__init__(name, "elf", Stats(
-            4, 10, 10, 9, 10, 12, 9))
+            1, 1, 1, -1, 0, 2, -1))
 
 class Goblin(Char):
     def __init__(self, name):
         super().__init__(name, "goblin", Stats(
-            3, 12, 4, 11, 7, 9, 9))
+            -1, 2, -60, 1, -3, -1, -1))
+
+def dead(c):
+    t = type(c)
+    if(issubclass(t, Stats)):
+        return c.HP <= 0 or c.INT <= 0
+    if(issubclass(t, Char)):
+        return dead(c.stats)
+    for ci in c:
+        if(alive(ci)):
+            return False
+    return True
+
+def alive(c):
+    return not dead(c)
 
 def die(c):
     t = type(c)
-    if(issubtype(t, Stats)):
+    if(issubclass(t, Stats)):
         c.HP = 0
         c.INV = 0
-    elif(issubtype(t, Char)):
+    elif(issubclass(t, Char)):
         die(c.stats)
         c.target = None
-    elif(isanysubtype(t, list, tuple)):
+    elif(isanysubclass(t, list, tuple)):
         for ci in c:
             die(ci)
     else:
