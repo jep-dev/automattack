d0f3d1a5880369870d5cdfcdf67530dcb25a8a11
jep-dev <john@xpcx.com>
2023-03-19 06:06:17 -0400

    Bug hunting

--- team.py
+++ team.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from util import *
 from char import *
 from time import sleep
 
@@ -8,31 +9,60 @@ __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
 
 class Group(list):
-    def transform(self, f, *args):
-        return [f(m, *args) for m in members]
-    def filter(self, f, *args):
-        return [m for m in members if f(m, *args)]
-    def filterout(self, f, *args):
-        return [m for m in members if not f(m, *args)]
+    def transform(self, fn):
+        return Group(fn(x) for x in self)
+    def life(self):
+        return Group(*[g for g in self if alive(g)])
+    def death(self):
+        return Group(*[g for g in self if not alive(g)])
+    def __init__(self, *lst):
+        super().__init__(l for l in lst)
+
+class Teams:
+    life = []
+    death = []
+    def __getitem__(self, k):
+        return self.life[k]
+    def __init__(self, *lst):
+        life = [l for l in lst if dead(l)]
+        death = [l for l in lst if not dead(l)]
+
+#def fn_id(x, *args, **kwargs):
+#    return x
+#def fn_true(x, *args, **kwargs):
+#    return True
+#def fn_false(x, *args, **kwargs):
+#    return False
+#def fn_negation(f):
+#    return lambda x, *args, **kwargs : not f(x)
+#
+#
+#def transform(lst, fn = fn_id, pred = fn_true):
+#    C = type(lst)
+#    if(issubclass(C, dict)):
+#        out = C()
+#        for k,v in lst.items():
+#            if(pred(v)):
+#                out[k] = f(v)
+#        return out
+#    if(isanysubclass(C, list, tuple)):
+#        out = C()
+#        for l in lst:
+#            if(pred(l)):
+#                out.append(l)
+#        return out
+#    return None
+
 
 def chars(c):
     out = Group()
-    if(issubclass(type(c), list)):
+    t = type(c)
+    if(isanysubclass(t, list, tuple)):
         for ci in c:
             out.extend(chars(ci))
-    elif(issubclass(type(c), tuple)):
-        for ci in c:
-            out.extend(chars(ci))
-    #elif(issubclass(type(c), Char)):
+    elif(issubclass(t, dict)):
+        for k,v in c.items():
+            out.append(v)
     else:
         out.append(c)
     return out
-
-def life(g):
-    return [gi for g in chars(g) if gi != None and alive(gi)]
-def death(g):
-    return [gi for g in chars(g) if gi == None or dead(gi)]
-
-
-class Team(Group):
-    pass
