0262b2e33a94713677c4f287ab4fd088e5345497
jep-dev <john@xpcx.com>
2023-04-04 05:47:07 -0400

    Added a few parsing functions: int, float, int-or-float

--- util.py
+++ util.py
@@ -5,6 +5,25 @@ __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
 
 def clamp(x, lo = 0, hi = 20):
-    if(lo > hi):
-        return min(max(hi, x), lo)
-    return min(max(lo, x), hi)
+    lo2 = min(lo, hi)
+    hi2 = max(lo, hi)
+    return min(max(lo2, x), hi2)
+
+def atoi(x):
+    try:
+        return int(x)
+    except ValueError:
+        return None
+
+def atof(x):
+    try:
+        return float(x)
+    except ValueError:
+        return None
+
+def aton(x):
+    f = atof(x)
+    i = atoi(x)
+    if(i == f):
+        return i
+    return f
