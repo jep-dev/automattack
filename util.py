eac36b0eb67521a272d5e67d516ac85148768b19
jep-dev <john@xpcx.com>
2023-04-04 21:20:32 -0400

    Added comparison functions, trim and compress for strings, a type conversion for mixed bag numbers, and a unit test

--- util.py
+++ util.py
@@ -4,10 +4,34 @@ __author__ = "John Petersen"
 __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
 
-def clamp(x, lo = 0, hi = 20):
+import math
+import re
+
+def ltcompare(l, r):
+    return l < r
+
+def compare(l, r, f = ltcompare):
+    if(l < r):
+        return -1
+    if(r < l):
+        return 1
+    return 0
+
+def ispsubclass(l, r):
+    if(issubclass(l, r)):
+        return not issubclass(r, l)
+    return False
+
+def clamp(x, lo = 0, hi = 1):
     lo2 = min(lo, hi)
     hi2 = max(lo, hi)
     return min(max(lo2, x), hi2)
+
+def trim(src, to = ''):
+    return re.sub(r'^\W+|\W+$', to, src)
+
+def compress(src, to = ''):
+    return re.sub(r'\W+', to, src)
 
 def atoi(x):
     try:
@@ -21,9 +45,43 @@ def atof(x):
     except ValueError:
         return None
 
-def aton(x):
+def aton(src):
+    x = src.lower()
+    x = ''.join(x.split())
     f = atof(x)
-    i = atoi(x)
-    if(i == f):
-        return i
-    return f
+    if('.' in x):
+        return f
+    if('inf' in x or 'nan' in x):
+        return f
+    return atoi(x)
+
+def test_util():
+    isa = lambda x,y : type(x) == y
+    isint = lambda x : isa(x, int)
+    isfloat = lambda x : isa(x, float)
+
+    ints = ['-1', '+1']
+    nones = ['', '.', '-', '+', '0x']
+    floats = ['-nan', '-inf', '-0.', '.0', 'inf', 'nan']
+
+    for n in nones:
+        assert atof(n) == None
+        assert atoi(n) == None
+        assert aton(n) == None
+
+    for n in ints:
+        assert isint(atoi(n))
+        assert isfloat(atof(n))
+        assert isint(aton(n))
+
+    for n in floats:
+        assert atoi(n) == None
+        assert isfloat(atof(n))
+        assert isfloat(aton(n))
+
+    return True
+
+if(__name__ == "__main__"):
+    #sys.exit(not test_util())
+    if(not test_util()):
+        sys.exit(1)
