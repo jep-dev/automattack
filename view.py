1274672deba3af4e3376eb07dcce95ec14eae44e
jep-dev <john@xpcx.com>
2023-04-07 11:35:23 -0400

    Switched from SDL2 to GTK3

--- view.py
+++ view.py
@@ -1,50 +1,28 @@
 #!/usr/bin/env python3
 
 import sys
-import sdl2.ext
 from time import sleep
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 
 __author__ = "John Petersen"
 __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
 
+class Frame(Gtk.Window):
+    def run(self):
+        self.win.show_all()
+        Gtk.main()
+    def __init__(self, t = ""):
+        super().__init__(title = t)
+        self.win = Gtk.Window()
+        self.win.connect("destroy", Gtk.main_quit)
 
-def init():
-    sdl2.ext.init()
-    RES = sdl2.ext.Resources(__file__, "share")
-    win = sdl2.ext.Window("Title", size=(640,480))
-    win.show()
-    fac = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-    sprite = fac.from_image(RES.get_path("test.png"))
-    renderer = fac.create_sprite_render_system(win)
-    sprite.position = 10,10
-    renderer.render(sprite)
-    sprite.position = 100,10
-    return [RES, win, fac, sprite]
-
-def run(win):
-    evs = sdl2.ext.get_events()
-    for ev in evs:
-        if ev.type == sdl2.SDL_QUIT:
-            return False
-        elif ev.type == sdl2.SDL_MOUSEBUTTONDOWN:
-            print('Mouse click')
-        elif ev.type == sdl2.SDL_MOUSEBUTTONUP:
-            print('Mouse release')
-    win.refresh()
-    return True
-
-def stop(win):
-    win.close()
-    sdl2.ext.quit()
-    return True
 
 def main():
-    args = sys.argv
-    res, win, fac, spr = init()
-    while(run(win)):
-        continue
-    stop(win)
+    Frame("Automattack").run()
     return 0
 
 if __name__ == "__main__":
