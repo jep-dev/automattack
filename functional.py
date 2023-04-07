b895efc1fe2e37b44294c5d7fd7036a5e6859b10
jep-dev <john@xpcx.com>
2023-04-07 11:12:36 -0400

    Added conjunction/disjunction/negation/composition

--- functional.py
+++ functional.py
@@ -4,9 +4,18 @@ __author__ = "John Petersen"
 __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
 
-from util import *
 import math
 import sys
+
+from util import *
+
+def ftransform(f, *x, **y):
+    t = type(f)
+    if(issubclass(t, list)):
+        return [fi(*x, **y) for fi in f]
+    elif(issubclass(t, tuple)):
+        return (fi(*x, **y) for fi in f)
+    return None
 
 def ltransform(f, x, *y, **z):
     t = type(x)
@@ -24,14 +33,6 @@ def rtransform(f, w, x, *y, **z):
         return (f(w, xi, *y, **z) for xi in x)
     return None
 
-def ftransform(f, *x, **y):
-    t = type(f)
-    if(issubclass(t, list)):
-        return [fi(*x, **y) for fi in f]
-    elif(issubclass(t, tuple)):
-        return (fi(*x, **y) for fi in f)
-    return None
-
 def lbind(f, *y, **z):
     return lambda x : f(*y, x, **z)
 
@@ -40,6 +41,19 @@ def rbind(f, *y, **z):
 
 def fnot(f, *y, **z):
     return lambda *x : not f(*x, *y, **z)
+
+def f_or(f, g):
+    return lambda x : f(x) or g(x)
+def fxor(f, g):
+    return lambda x : bool(f(x)) ^ bool(g(x))
+def fand(f, g):
+    return lambda x : f(x) and g(x)
+
+def fcompose(f, g):
+    return lambda x : f(g(x))
+
+def fcompare(f, g):
+    return lambda *y, **z : compare(f(*y, **z), g(*y, **z))
 
 def ffilter(f, x, *y, **z):
     t = type(x)
@@ -55,36 +69,33 @@ def ffilter(f, x, *y, **z):
     return [None, None]
 
 def finclude(f, x, *y, **z):
-    return ffilter(f, x, *y, **z)[0]
-
-def fexclude(f, x, *y, **z):
     return ffilter(f, x, *y, **z)[1]
 
+def fexclude(f, x, *y, **z):
+    return ffilter(f, x, *y, **z)[0]
+
 def test_functional():
-    f = lambda x,*y,**z: math.isfinite(x)
-    g = fnot(f)
-    x0 = math.nan
-    x1 = math.inf
-    x2 = 0.0
-    xn = [-x0, -x1, -x2, x2, x1, x0]
-    lx = len(xn)
-    print(f'X[{lx}] = {xn}')
-    for xi in xn:
-        assert f(xi) == (not g(xi))
+    m = 12
+    u0 = [*range(0, 12)]
+    t0 = [*range(0, 12, 3)]
+    t1 = [*range(1, 12, 3)]
+    t2 = [*range(2, 12, 3)]
+    y = lambda l,r : lambda i : ((i%l+l)%l == r)
 
-    u,v = ffilter(f, xn)
-    for uj in u:
-        assert g(uj)
-    lu = len(u)
-    print(f'U[{lu}] = {u}')
+    even = y(2,0)
+    odd = fnot(even)
+    for i in u0:
+        assert odd(i) == (not even(i))
+    alt = [bool(i&1) for i in u0]
+    assert ltransform(odd, u0) == alt
+    alt = [not (i&1) for i in u0]
+    assert ltransform(even, u0) == alt
 
-    for vk in v:
-        assert f(vk)
-    lv = len(v)
-    print(f'V[{lv}] = {v}')
-    assert lu + lv == lx
-
+    v0,v1 = ffilter(odd, u0)
+    assert v0 == [*range(0, 12, 2)]
+    assert v1 == [*range(1, 12, 2)]
     return True
+
 
 if(__name__ == "__main__"):
     print('Testing functional.py;')
