cc5632c890f6e3aa62e191b4dba096962958d53d
jep-dev <john@xpcx.com>
2023-04-07 13:28:18 -0400

    Moved stuff here

--- streams.py
+++ streams.py
@@ -5,14 +5,45 @@ __version__ = "0.0.1"
 __license__ = "GNU General Public License 3.0"
 
 import io
+import re
+import sys
+
 from util import *
 from timing import *
+from functional import *
 
-def emptyline(s):
-    return len(s) == 0 or s[0] == '#'
+def yesman(*y, **z):
+    return True
+def iscomment(src, *y, **z):
+    return re.match(r'^\s*#', src) != None
 
-def lines(fname):
+def trim(src, to = ''):
+    return re.sub(r'^\s+|\s+$', to, src)
+def compress(src, to = ' ', pat = r'\S+'):
+    return to.join(re.findall(pat, src))
+def noncomment(src):
+    return re.sub(r'\s*#.*', '', src)
+
+
+def lines(fname, dest = [], pred = yesman, fn = identity):
     f = io.open(fname)
-    out = [line for line in f if not emptyline(line)]
+    for l in f:
+        if(pred(l, *y, **z)):
+            dest.append(l)
     f.close()
-    return out
+    return dest
+
+if(__name__ == '__main__'):
+    i = 0
+    coms = []
+    noncoms = []
+    fname = 'share/test.txt'
+    lines(fname, coms, iscomment)
+    lines(fname, noncoms, fnot(iscomment))
+    #
+
+    for line in dest:
+        i = i + 1
+        print(f'{i}. "{line}"')
+    sys.exit(0)
+
